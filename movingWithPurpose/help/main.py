fe31ebe899dd7ac3b4c8182dc2a9116391155e89
Christian Winwood <christian.winwood@aggiemail.usu.edu>
2020-09-24 15:13:07 -0600

    help

---
+++ movingWithPurpose/help/main.py
@@ -0,0 +1,53 @@
+from movingDot import MovingDot
+from importService import ImportService
+from movingDotService import MovingDotService
+import time
+from presets import Presets
+import pygame
+from ackerman import Ackerman
+from ackermanService import AckermanService
+import math
+
+def checkArrived(ackerman, dot, endLocal):
+    global keepRunning
+
+    tempX = endLocal[0] - ackerman.currentLocation[0]
+    tempY = endLocal[1] - ackerman.currentLocation[1]
+    if (tempX < 1 and tempX > -1) and (tempY < 1 and tempY > -1):
+        time.sleep(3)
+        keepRunning = False
+
+
+pygame.init()
+
+importService = ImportService()
+importService.setFile("config_assignment3.json")
+presets = importService.getPresets()
+
+endLocal = presets.endLocation
+startLocal = presets.startLocation
+#startLocal = [75,75]
+
+dot = MovingDot(currentLocation=startLocal)
+dotService = MovingDotService()
+
+ackerman = Ackerman(currentLocation=startLocal, maxSteeringAngle=math.radians(presets.maxSteeringAngle), facingDirection=presets.facingDirection,velocity=5)
+ackermanService = AckermanService()
+
+keepRunning = True
+screen = pygame.display.set_mode((1000, 1000))
+
+while keepRunning:
+    screen.fill((0,0,0))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            keepRunning = False
+            break
+#    dotService.move(dot, endLocal)
+#    dotService.draw(screen, dot)
+    ackermanService.move(ackerman, endLocal)
+    ackermanService.draw(screen, ackerman)
+    pygame.display.update()
+    checkArrived(ackerman, dot, endLocal)
+    time.sleep(.05)
+pygame.quit()
