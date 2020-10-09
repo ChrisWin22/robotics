04c3ec7aa1a73fe299dc7ee2b6c7dceec9c8ea7b
Christian Winwood <christianwinwood@gmail.com>
2020-10-08 23:50:34 -0600

    added submission folder

---
+++ movingWithPurpose/Submission/presets.py
@@ -0,0 +1,8 @@
+class Presets:
+
+    def __init__(self, startLocation = [0,0], endLocation=[0,0], facingDirection=0, maxVelocity=0, maxSteeringAngle=0):
+        self.startLocation = startLocation
+        self.endLocation = endLocation
+        self.facingDirection = facingDirection
+        self.maxVelocity = maxVelocity
+        self.maxSteeringAngle = maxSteeringAngle
