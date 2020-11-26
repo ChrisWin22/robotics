1537fc222fe4a333898665d4e5d121712bb2169a
Christian Winwood <christian.winwood@aggiemail.usu.edu>
2020-11-25 22:03:58 -0700

    robot moves from top left to dock

--- finalProject/main.py
+++ finalProject/main.py
@@ -6,15 +6,30 @@ from models.ackerman import Ackerman
 from services.ackermanService import AckermanService
 from services.rinkService import RinkService
 import math
+from models.rinks import rink
 
-def checkArrived(ackerman, dot, endLocal):
-    global keepRunning
 
+def checkArrived(ackerman, endLocal):
     tempX = endLocal[0] - ackerman.currentLocation[0]
     tempY = endLocal[1] - ackerman.currentLocation[1]
     if (tempX < 1 and tempX > -1) and (tempY < 1 and tempY > -1):
         time.sleep(3)
-        keepRunning = False
+        return True
+    return False
+
+def enterRink(ackerman, rink, rink_service, screen, presets, ackermanService):
+    entranceLocal = presets.dock 
+    while True:
+        screen.fill((0,0,0))
+        rink_service.draw_dock(rink,presets.dock[0],presets.dock[1])
+        rink_service.drawRink(rink)
+        ackermanService.move(ackerman, entranceLocal)
+        ackermanService.draw(screen, ackerman)
+        pygame.display.update()
+        if checkArrived(ackerman, entranceLocal):
+            return
+
+
 
 def main():
     pygame.init()
@@ -28,14 +43,14 @@ def main():
     startLocal = presets.startLocation
     #startLocal = [75,75]
 
-    ackerman = Ackerman(currentLocation=startLocal, maxSteeringAngle=math.radians(presets.maxSteeringAngle), facingDirection=presets.facingDirection,velocity=5)
+    ackerman = Ackerman(currentLocation=startLocal, maxSteeringAngle=math.radians(presets.maxSteeringAngle), facingDirection=presets.facingDirection,velocity=3)
     ackermanService = AckermanService()
 
     keepRunning = True
     mapping = rink_service.determine_rink(presets.name)
     screen = mapping.display
 
-    
+    enterRink(ackerman, mapping, rink_service, screen, presets, ackermanService)
 
     while keepRunning:
         screen.fill((0,0,0))
@@ -46,9 +61,9 @@ def main():
                 keepRunning = False
                 break
         # ackermanService.move(ackerman, endLocal)
-        # ackermanService.draw(screen, ackerman)
+        ackermanService.draw(screen, ackerman)
         pygame.display.update()
-        # checkArrived(ackerman, dot, endLocal)
+        # checkArrived(ackerman, endLocal)
         # time.sleep(.05)
     pygame.quit()
 
